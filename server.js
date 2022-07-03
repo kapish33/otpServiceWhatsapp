be019014c10ff432c1183c7d0b7a2141e3440a7d
Kapish Singh <kapish.singh@bijak.in>
2022-07-03 13:32:21 +0530

    enganced

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const PORT = process.env.PORT || 3001;
+const app = require('express')();
+const PORT = process.env.PORT || 4000;
+const server = require('http').createServer(app);
 
-const io = require("socket.io")(server, {
+const io = require('socket.io')(server, {
   cors: {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders:
-      "Content-Type, Authorization, X-Requested-With, X-CSRF-Token",
+      'Content-Type, Authorization, X-Requested-With, X-Socket-ID',
     credentials: true,
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("What is Socket ID", socket);
-  socket.on("chat", (payload) => {
-    console.log("Chat", payload);
-    io.emit("chat", payload);
+io.on('connect', (socket) => {
+  console.log('a user connected');
+  // console.log('what is socket', socket);
+  // make any event emit to all clients
+  socket.on('chat', (payload) => {
+    io.emit('chat', payload);
+  });
+  socket.on('askForName', () => {
+    io.emit('askForName');
+  });
+  socket.on('getGroupNames', async (names) => {
+    await io.emit('getGroupNames', names);
+  });
+  socket.on('getSelectedGropuNames', async (names) => {
+    await io.emit('getSelectedGropuNames', names);
   });
 });
+
 server.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log('listening on port', PORT);
 });
